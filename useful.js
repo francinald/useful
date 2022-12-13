8fb30d04bec2d4cb95beb61e1daf7934dbe39efc
Francinaldo <cinaldo.s.gomes@gmail.com>
2022-12-13 11:54:44 -0300

    reparando bugs e adicionando metados novos

--- useful.js
+++ useful.js
@@ -1,12 +1,27 @@
 export default class Useful{
+    static get(url){
+        return this.httpRequest("GET",url)
+    }
+    static post(url,dados){
+        return this.httpRequest("POST",url,dados)
+    }
+    static put(){
+        return this.httpRequest("PUT",url,dados)
+    }
+    static delete(){
+        return this.httpRequest("DELETE",url,dados)
+    }
     static httpRequest(method,url,dados=null){
         return new Promise((resolve,reject)=>{
             const xhr = new XMLHttpRequest()
             xhr.open(method.toUpperCase(),url,true)
             xhr.setRequestHeader("Content-Type","application/json")
+            xhr.setRequestHeader('Access-Control-Allow-Origin','*')
             xhr.onreadystatechange = ()=>{
-                if(xhr.responseType === 4 && xhr.status < 304){
-                    resolve(JSON.parse(xhr.responseText))
+                if(xhr.readyState === 4 && xhr.status < 304){
+                    if(xhr.status === 200){
+                        resolve(JSON.parse(xhr.responseText))
+                    }
                 }
             }
             xhr.onerror = err => {
