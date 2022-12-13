6efe11d75939e6af6f28e3a01f9921618f22159f
Francinaldo <cinaldo.s.gomes@gmail.com>
2022-12-13 15:36:53 -0300

    adicionando nova funcionalidade getPhoto getForm

--- useful.js
+++ useful.js
@@ -1,4 +1,40 @@
 export default class Useful{
+    static listenner(element,events,cb){
+        events.split(" ").forEach(event=>{
+            element.addEventListener(event,cb,false)
+        })
+    }
+    static getFormUse(form){
+        const user = {}
+        ;[...form.elements].forEach(field => {
+            if(field.name){
+                if(field.type === "radio"){
+                    if(field.checked){
+                        user[field.name] = field.value
+                    }
+                }else if(field.type === "checkbox"){
+                    user[field.name] = field.checked
+                }else{
+                    user[field.name] = field.value
+                }
+            }
+        })
+        return user
+    }
+    static getPhoto(form){
+        return new Promise((resolve,reject)=>{
+            const elements = [...form.elements].filter(files => files.type === "file")
+            const photo = elements[0].files[0]
+            const file = new FileReader()
+            file.onload = ()=>{
+                resolve(file.result)
+            }
+            file.onerror = err => {
+                reject({err})
+            }
+            file.readAsDataURL(photo)
+        })
+    }
     static get(url){
         return this.httpRequest("GET",url)
     }
